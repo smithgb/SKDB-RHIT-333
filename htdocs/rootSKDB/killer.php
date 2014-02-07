4071c2764b9de31f66df1951c855b7b3d4eb2959
vattercw <vattercw@rose-hulman.edu>
2014-02-07 12:40:24 -0500

    Wut? Moar stuffz? Mayne datz tew mutch!

--- htdocs/rootSKDB/killer.php
+++ htdocs/rootSKDB/killer.php
@@ -1,16 +1,33 @@
 <?php
-	$SID = $_GET['SID'];
+$SID = $_GET['SID'];
 
-	$conn = mysql_connect('localhost', 'root', '')or die(mysql_error());
+$conn = mysql_connect('localhost', 'root', '')or die(mysql_error());
 
-	mysql_select_db('skdb') or die(mysql_error());
+mysql_select_db('skdb') or die(mysql_error());
 
-	$result = mysql_query("SELECT * FROM serialkiller WHERE SID=".$SID."" );
+$result = mysql_query("SELECT * FROM serialkiller WHERE SID=".$SID."" );
 
+$row = mysql_fetch_array($result);
 
-	$row = mysql_fetch_array($result);
+$birthplace = mysql_query("SELECT * FROM location WHERE Zipcode=".$row['Birthplace']."" );
 
-	$picture = $row['ImagePath'];
+$bp = mysql_fetch_array($birthplace);
+
+$lived = mysql_query("SELECT * FROM location WHERE Zipcode=".$row['Lived']."" );
+
+$live = mysql_fetch_array($lived);
+
+$category = mysql_fetch_array(mysql_query("SELECT * FROM category WHERE CID=".$row['Category']."" ));
+
+$motive = mysql_fetch_array(mysql_query("SELECT * FROM motive WHERE MID=".$row['Motive']."" ));
+
+$picture = $row['ImagePath'];
+
+if($row['Gender'] == "M"){
+	$gender = "Male";
+}else{
+	$gender = "Female";
+}
 
 ?>
 
@@ -70,23 +87,65 @@
 	<div>
 
 
-	</body>
-	</html>
+<!-- Not making columns like I wanted -->
 
-	<div style="padding-left: 5%">
-	<?php
+	<div class="container">
+		<div class="row">
+			<div class="span4">
+				<?php
 
-	echo "<img src= ".$picture." height = 400px><br />";
+				echo "<img src= ".$picture." height = 400px><br />";
 
-	echo "<h2>" . $row['FName'] . " " . $row['LName'] . "</h2>";
+				echo "<h1>" . $row['FName'] . " " . $row['MName'] . " " . $row['LName'] . "</12>";
 
-	?>
+				echo "<h2>Alias(es): " . $row['Aliases'] . "</h2>";
+
+				echo "<h3>Number of Victims: " . $row['NumVictims'] . "</h3>";
+				
+				echo "<h4>Category: <a href = '/rootSKDB/category.php?CID=" . $category['CID'] . "'>" . $category['Name'] . "</a></h4>";
+
+				echo "<h4>Motive: <a href = '/rootSKDB/motive.php?MID=" . $motive['MID'] . "'>" . $motive['Name'] . "</h4> </a><br />";
+
+
+				echo "<h4>Gender: " . $gender . "</h4>";
+
+				echo "<h4>Born: " . $row['Birthday'] . "</h4>";
+
+				//TODO find name of birthplace and lived (if null for birthplace say Unknown for Lived say No Permanent Residence)
+				echo "<h4>Birthplace: " . $bp['City'] . ', ' . $bp['State'] . ', '. $bp['Country'] . '</h4>';
+
+				echo "<h4>Lived: " . $live['City'] . ', ' . $live['State'] . ', '. $live['Country'] . '</h4>';
+
+				echo "<h4>Years of Activity: " . $row['YearsofActivity'] . "</h4>";
+
+				//if not arrested say Never Apprehended
+
+				echo "<h4>Date Arrested: " . $row['DateofArrest'] . "</h4>";
+
+				echo "<h4>Punishment: " . $row['Punishment'] . "</h4>";
+
+				//if not dead say Alive
+
+				echo "<h4>Died: " . $row['Died'] . "</h4><br />";
+
+				
+
+				?>
+			</div>  
+			<div class="span6">
+				<?php
+
+				echo "<p>" . $row['Biography'] . "<p>";
+
+
+
+				?>
+			</div>
+		</div>  
 	</div>
 
-
-
-
-
+</body>
+</html>
 
 
 
