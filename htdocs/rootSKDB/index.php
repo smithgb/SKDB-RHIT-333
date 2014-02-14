f3f9bc4414d1329da9a246f3822d4f302a4a951c
smithgb <smithgb@rose-hulman.edu>
2014-02-14 14:16:29 -0500

    Global Navbar Implemented

---
+++ htdocs/rootSKDB/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link type = "text/css" rel = "stylesheet" href="../stylesheets/bootstrap.css" />
+	<script type="text/javascript" src='../javascript/jquery.js'></script>
+	<script type= 'text/javascript' src="../javascript/bootstrap.js"></script>
+	<title>SKDB</title>
+</head>
+<body>
+
+	<?php require("navbar.html"); ?>
+
+</body>
+</html>
