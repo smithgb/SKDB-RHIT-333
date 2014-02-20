b83e708c148fa1dbdc406869c5339f61130745c4
vattercw <vattercw@rose-hulman.edu>
2014-02-20 10:09:18 -0500

    disclaimer

---
+++ htdocs/rootSKDB/disclaimer.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<?php require('partials/bootstrapTheme.html') ?>
+</head>
+
+<title>Disclaimer</title>
+
+<body>
+	<?php require("partials/navbar.html"); ?>
+	<div class = 'container'>
+	<p>This website is a student project for the Rose-Hulman Institute of Technology computer science department Introduction to Databases course. It is in no way endorsed by the institution or the professor by which the project was assigned.
+
+	We do not own or claim to own any of the information in the biographical or description sections for each of our entries.</p>
