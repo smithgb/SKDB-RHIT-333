fcdbb9eb156646bf0ddca82c16ed30927ee6b231
vattercw <vattercw@rose-hulman.edu>
2014-02-20 16:15:42 -0500

    stupid shit

---
+++ htdocs/rootSKDB/azdisorder.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<?php require('partials/bootstrapTheme.html') ?>
+	<title>SKDB</title>
+</head>
+<body>
+	
+	<?php require("partials/navbar.html"); ?>
+
+	<div class="container">
+		<div class="row vertical-center-row">
+			<div class="col-lg-40">
+				<div class="row ">
+					<div class="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-4 col-xs-offset-4">
+						<br />
+						<?php
+
+						$conn = mysql_connect('localhost', 'root', '')or die(mysql_error());
+
+						mysql_select_db('skdb') or die(mysql_error());
+
+						$result = mysql_query("SELECT * FROM disorder GROUP BY Name ASC");
+
+						$previous = null;
+						while ($row = mysql_fetch_array($result))
+						{
+							
+							echo "<a href = '/rootSKDB/disorder.php?DID=" . $row['DID'] . "'><h4>" . $row['Name'] . "</h4></a>";
+						} 
+
+						mysql_close($conn);
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</br>
+</br>
+
+</body>
+</html>
--- htdocs/rootSKDB/disclaimer.php
+++ htdocs/rootSKDB/disclaimer.php
@@ -9,6 +9,8 @@
 	<?php require("partials/navbar.html"); ?>
 	<div class = 'container'>
 	<br />
+	<h1>We're Sorry</h1>
+	<br />
 	<p>This website is a student project for the Rose-Hulman Institute of Technology computer science department Introduction to Databases course. It is in no way endorsed by the institution or the professor by which the project was assigned. </p>
 
 	<p>We do not own or claim to own any of the information in the biographical or description sections for each of our entries.</p>
